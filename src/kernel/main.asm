8fb0ab0b9b808a895ada061d6bbad5c66ece2823
Thiago da Conceição <thiagogoisconceicao@gmail.com>
2024-01-24 15:59:07 -0300

    new files

---
+++ src/kernel/main.asm
@@ -0,0 +1,56 @@
+org 0x7C00
+
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+inicio:
+    
+    jmp main
+
+caralho: 
+
+    push si
+    push ax
+
+.loop:
+  
+    lodsb
+    or al, al
+    jz .pronto
+
+    mov ah, 0x0e
+    int 0x10
+    
+    jmp .loop
+
+.pronto:
+
+    pop ax
+    pop si
+    ret 
+
+main:
+    
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    mov ss, ax
+    mov sp, 0x7C00
+
+    mov si, msg_caralho
+    call caralho
+
+    hlt
+
+.halt:
+    jmp .halt
+
+
+msg_caralho: db 'ola calvo/caralho', ENDL, 0
+
+times 510-($-$$) db 0
+dw 0AA55h
+
+
